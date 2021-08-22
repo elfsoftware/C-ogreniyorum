37690416ea71e746e6315674c209628231c13330
kg <men.low01@hotmail.com>
2021-08-22 21:43:25 +0300

    Csecondproject

---
+++ secondproject/secondproject/main.c
@@ -0,0 +1,17 @@
+//
+//  main.c
+//  secondproject
+//
+//  Created by Kübra Gönülal on 22.08.2021.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    for (int i=0; i<=100; i++) {
+        if (i%5==0) {
+            printf("%d\n",i);
+        }
+    }
+    return 0;
+}
